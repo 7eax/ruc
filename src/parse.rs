b9f2034d48fadc4d0d1d940cb45f85914ba96466
れあ命令 <compiler-develop@proton.me>
2026-03-17 17:50:19 +0900

    Add macro for refactor

--- src/parse.rs
+++ src/parse.rs
@@ -22,93 +22,142 @@ impl Define {
 
 impl Expr {
     pub fn parse(source: &str) -> Result<Expr, String> {
-        let token = source.trim();
-        if let Some(token) = token.strip_prefix("let ") {
-            if let Ok((name, value)) = once!(token, "=") {
-                Ok(Expr::Let(Box::new(Expr::parse(&name)?), Box::new(Expr::parse(&value)?)))
-            } else {
-                Ok(Expr::Let(Box::new(Expr::parse(token)?), Box::new(Expr::Undefined)))
+        let x = source.trim();
+        macro_rules! surround {
+            ($ls: literal, $x: expr, $rs: literal) => {
+                $x.strip_prefix($ls).and_then(|x| x.strip_suffix($rs))
             }
-        } else if let Some(token) = token.strip_prefix("if ") {
-            if let Ok((cond, body)) = once!(token, "then") {
-                if let Ok((then, els)) = once!(&body, "else") {
-                    Ok(Expr::If(Box::new(Expr::parse(&cond)?), Box::new(Expr::parse(&then)?), Some(Box::new(Expr::parse(&els)?))))
+            ($x: expr, $ls: literal, $rs: literal) => {
+                $x.strip_suffix($rs).and_then(|x| x.rsplit_once($ls)) 
+            }
+        }
+
+        if let Some(x) = x.strip_prefix("let ") {
+            if let Ok((name, value)) = once!(x, "=") {
+                Ok(Expr::Let(
+                    Box::new(Expr::parse(&name)?), 
+                    Box::new(Expr::parse(&value)?)
+                ))
+            } else {
+                Ok(Expr::Let(
+                    Box::new(Expr::parse(x)?),
+                    Box::new(Expr::Undefined)
+                ))
+            }
+        } else if let Some(x) = x.strip_prefix("if ") {
+            if let Ok((cond, body)) = once!(x, "then") {
+                if let Ok((then, r#else)) = once!(&body, "else") {
+                    Ok(Expr::If(
+                        Box::new(Expr::parse(&cond)?), 
+                        Box::new(Expr::parse(&then)?), 
+                        Some(Box::new(Expr::parse(&r#else)?)
+                    )))
                 } else {
-                    Ok(Expr::If(Box::new(Expr::parse(&cond)?), Box::new(Expr::parse(&body)?), None))
+                    Ok(Expr::If(
+                        Box::new(Expr::parse(&cond)?),
+                        Box::new(Expr::parse(&body)?),
+                        None
+                    ))
                 }
             } else {
                 Err(format!("parse \"if\" but \"then\" not found: {source}"))
             }
-        } else if let Some(token) = token.strip_prefix("while ") {
-            if let Ok((cond, body)) = once!(token, "do") {
-                Ok(Expr::While(Box::new(Expr::parse(&cond)?), Box::new(Expr::parse(&body)?)))
+        } else if let Some(x)  = x.strip_prefix("while ") {
+            if let Ok((condition, loop_body)) = once!(x, "do") {
+                Ok(Expr::While(
+                    Box::new(Expr::parse(&condition)?), 
+                    Box::new(Expr::parse(&loop_body)?)
+                ))
             } else {
                 Err(format!("parse \"while\" but \"do\" not found: {source}"))
             }
-        } else if let Some(token) = token.strip_prefix("{").and_then(|token| token.strip_suffix("}")) {
+        } else if let Some(x) = surround!("{", x, "}") {
             let mut block = vec![];
-            for line in tokenize(token, "\n")? {
-                let (line, _) = once!(&line, ";").unwrap_or((line, String::new()));
-                if !line.trim().is_empty() { block.push(Expr::parse(&line)?); }
+            for line in tokenize(x, "\n")? {
+                let (line, _) = once!(&line, ";")
+                    .unwrap_or((line, String::new()));
+                if !line.trim().is_empty() { 
+                    block.push(Expr::parse(&line)?); 
+                }
             }
             Ok(Expr::Block(block))
+        } else if x == "break" { 
+            Ok(Expr::Break(Box::new(Expr::Undefined))) 
+        } else if x == "return" { 
+            Ok(Expr::Return(Box::new(Expr::Undefined))) 
+        } else if let Some(x) = x.strip_prefix("break ") { 
+            Ok(Expr::Break(Box::new(Expr::parse(&x)?))) 
+        } else if let Some(x) = x.strip_prefix("return ") { 
+            Ok(Expr::Return(Box::new(Expr::parse(&x)?))) 
         } 
-        else if token == "break" { Ok(Expr::Break(Box::new(Expr::Undefined))) }
-        else if token == "return" { Ok(Expr::Return(Box::new(Expr::Undefined))) } 
-        else if let Some(token) = token.strip_prefix("break ") { Ok(Expr::Break(Box::new(Expr::parse(&token)?))) } 
-        else if let Some(token) = token.strip_prefix("return ") { Ok(Expr::Return(Box::new(Expr::parse(&token)?))) } 
         
-        else if let Ok(operator) = parse_oprator(token) { Ok(operator) }
-        else if let Some(ptr) = token.strip_prefix("*") { Ok(Expr::Derefer(Box::new(Expr::parse(ptr)?))) } 
-        else if let Some(ptr) = token.strip_prefix("&") {
-            if let Ok(name) = Name::new(ptr) { Ok(Expr::Pointer(name)) } 
-            else if let Ok(Expr::Derefer(ptr)) = Expr::parse(ptr) { Ok(*ptr.clone()) } 
-            else { Err(format!("invalid reference: {ptr}")) }
-        } 
-        else if token.starts_with("\"") && token.ends_with("\"") { Ok(Expr::String(token.to_owned())) } 
-        else if let Some(expr) = token.strip_prefix("(").and_then(|x| x.strip_suffix(")")) { Expr::parse(expr) } 
-        else if let (true, Some(expr)) = (token.contains("("), token.strip_suffix(")")) {
-            let (name, args) = ok!(expr.split_once("("))?;
-            let args = tokenize(&args, ",")?.iter().map(|x| Expr::parse(x));
-            Ok(Expr::Call(Box::new(Expr::parse(&name)?), args.collect::<Result<Vec<_>, String>>()?))
-        } 
-        else if let (true, Some(expr)) = (token.contains("["), token.strip_suffix("]")) {
-            let (arr, idx) = ok!(expr.rsplit_once("["))?;
-            let offset = Expr::Mul(Box::new(Expr::parse(idx)?), Box::new(Expr::Integer(8)));
-            Ok(Expr::Derefer(Box::new(Expr::Add(Box::new(Expr::parse(arr)?), Box::new(offset)))))
-        } 
-        else if let Ok(literal) = token.parse::<i64>() { Ok(Expr::Integer(literal)) }
-        else if let Ok(literal) = token.parse::<bool>() { Ok(Expr::Integer(if literal { 1 } else { 0 })) } 
-        else { Ok(Expr::Variable(Name::new(token)?)) }
+        else if let Ok(operator) = parse_oprator(x) { Ok(operator) }
+        else if let Some(pointer) = x.strip_prefix("*") { 
+            Ok(Expr::Derefer(Box::new(Expr::parse(pointer)?))) 
+        } else if let Some(ptr) = x.strip_prefix("&") {
+            if let Ok(name) = Name::new(ptr) { 
+                Ok(Expr::Pointer(name)) 
+            } else if let Ok(Expr::Derefer(ptr)) = Expr::parse(ptr) { 
+                Ok(*ptr.clone()) 
+            } else { 
+                Err(format!("invalid reference: {ptr}")) 
+            }
+        }  else if x.starts_with("\"") && x.ends_with("\"") { 
+            Ok(Expr::String(x.to_owned())) 
+        } else if let Some(expr) = surround!("(", x, ")") { 
+            Expr::parse(expr)
+        } else if let Some((func, args)) = surround!(x, "(", ")") {
+            let args = tokenize(&args, ",")?.
+                iter().map(|x| Expr::parse(x));
+            Ok(Expr::Call(
+                Box::new(Expr::parse(&func)?), 
+                args.collect::<Result<Vec<_>, String>>()?
+            ))
+        } else if let Some((arr, idx)) = surround!(x, "[", "]") {
+            let offset = Expr::Mul(
+                Box::new(Expr::parse(idx)?), 
+                Box::new(Expr::Integer(8)
+            ));
+            Ok(Expr::Derefer(Box::new(Expr::Add(
+                Box::new(Expr::parse(arr)?),
+                Box::new(offset)
+            ))))
+        } else if let Ok(literal) = x.parse::<i64>() { 
+            Ok(Expr::Integer(literal)) 
+        } else if let Ok(literal) = x.parse::<bool>() { 
+            Ok(Expr::Integer(if literal { 1 } else { 0 })) 
+        } else { 
+            Ok(Expr::Variable(Name::new(x)?))
+        }
     }
 }
 
 fn parse_oprator(source: &str) -> Result<Expr, String> {
-    let tokens: Vec<String> = tokenize(source, SPACE)?;
+    let xs: Vec<String> = tokenize(source, SPACE)?;
 
-    let op = ok!(tokens.len().checked_sub(2))?;
-    let lhs_term = &ok!(tokens.get(..op))?.join(SPACE);
-    let rhs_term = &ok!(tokens.get(op + 1..))?.join(SPACE);
+    let op = ok!(xs.len().checked_sub(2))?;
+    let lhs = &ok!(xs.get(..op))?.join(SPACE);
+    let rhs = &ok!(xs.get(op + 1..))?.join(SPACE);
 
-    let op = ok!(tokens.get(op))?.as_str();
-    let lhs = Box::new(Expr::parse(lhs_term)?);
-    let rhs = Box::new(Expr::parse(rhs_term)?);
+    let op = ok!(xs.get(op))?.as_str();
+    let lhs = Box::new(Expr::parse(lhs)?);
+    let rhs = Box::new(Expr::parse(rhs)?);
 
-    Ok(match op {
-        "+" => Expr::Add(lhs, rhs),
-        "-" => Expr::Sub(lhs, rhs),
-        "*" => Expr::Mul(lhs, rhs),
-        "/" => Expr::Div(lhs, rhs),
-        "%" => Expr::Mod(lhs, rhs),
-        "==" => Expr::Eql(lhs, rhs),
-        "!=" => Expr::NotEq(lhs, rhs),
-        ">" => Expr::Gt(lhs, rhs),
-        "<" => Expr::Lt(lhs, rhs),
-        ">=" => Expr::GtEq(lhs, rhs),
-        "<=" => Expr::LtEq(lhs, rhs),
-        "&" => Expr::And(lhs, rhs),
-        "|" => Expr::Or(lhs, rhs),
-        "^" => Expr::Xor(lhs, rhs),
-        op => return Err(format!("unknown operator: {op}")),
-    })
+    match op {
+        "+" => Ok(Expr::Add(lhs, rhs)),
+        "-" => Ok(Expr::Sub(lhs, rhs)),
+        "*" => Ok(Expr::Mul(lhs, rhs)),
+        "/" => Ok(Expr::Div(lhs, rhs)),
+        "%" => Ok(Expr::Mod(lhs, rhs)),
+        "==" => Ok(Expr::Eql(lhs, rhs)),
+        "!=" => Ok(Expr::NotEq(lhs, rhs)),
+        ">" => Ok(Expr::Gt(lhs, rhs)),
+        "<" => Ok(Expr::Lt(lhs, rhs)),
+        ">=" => Ok(Expr::GtEq(lhs, rhs)),
+        "<=" => Ok(Expr::LtEq(lhs, rhs)),
+        "&" => Ok(Expr::And(lhs, rhs)),
+        "|" => Ok(Expr::Or(lhs, rhs)),
+        "^" => Ok(Expr::Xor(lhs, rhs)),
+        op => Err(format!("unknown operator: {op}")),
+    }
 }
